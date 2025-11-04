785ef4b5fe34a4a1571721465a456aecc42ffcdb
jecayonm8 <jecm0468@gmail.com>
2025-11-04 08:50:30 -0500

    Adelanto Guia: Maquetacion y estilos CSS, paso 13. Estilos de inputs en el formulario.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-advanzedProyect');
+  
 }
