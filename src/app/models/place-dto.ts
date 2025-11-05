2bb5b25e1c338b48053184cd4e347aad06e93416
jecayonm8 <jecm0468@gmail.com>
2025-11-04 23:01:56 -0500

    feat: implementación de DTOs y servicio para places

---
+++ src/app/models/place-dto.ts
@@ -0,0 +1,41 @@
+export interface AccommodationDTO {
+  title: string;
+  price: number;
+  photo_url: string;
+  average_rating: number;
+  city: string;
+}
+
+export type AccommodationType =
+  | 'HOUSE'
+  | 'APARTMENT'
+  | 'FARM';
+
+export type Amenities =
+  | 'WIFI'
+  | 'PARKING_AND_FACILITIES'
+  | 'SERVICES'
+  | 'BEDROOM_AND_LAUNDRY'
+  | 'SERVICE_ANIMALS_ALLOWED'
+  | 'FREEZER'
+  | 'HOT_WATER'
+  | 'KITCHEN'
+  | 'ENTERTAINMENT';
+
+export interface CreateAccommodationDTO {
+  title: string;
+  description: string;
+  price: number;
+  picsUrl: string[];
+  accommodationType: AccommodationType;
+  capacity: number;
+  country: string;
+  department: string;
+  city: string;
+  neighborhood?: string;
+  street?: string;
+  postalCode: string;
+  amenities: Amenities[];
+  latitude: number;
+  longitude: number;
+}
